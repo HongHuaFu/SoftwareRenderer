c522adbca28744f85844344e54f6d800c5a26349
HongHuaFu <qiutanguu@gmail.com>
2019-12-19 11:49:11 +0800

    add bresenham draw line func.

--- Dazed/Rasterize.cpp
+++ Dazed/Rasterize.cpp
@@ -32,3 +32,54 @@ void Rasterize::DrawLineDDA(const vec2i & p0,const vec2i & p1,const Color & col)
 		SetPixel(round(x),round(y),col);
 	}
 }
+
+void Rasterize::DrawLine(const vec2i & p0,const vec2i & p1,const Color & col)
+{
+	int x0 = p0.x; 
+	int x1 = p1.x;
+	int y0 = p0.y; 
+	int y1 = p1.y;
+	bool steep = false;
+	if (std::abs( x0 - x1 ) < std::abs( y0 - y1 ))
+	{
+		std::swap( x0 , y0 );
+		std::swap( x1 , y1 );
+		steep = true;
+	}
+
+	if (x0 > x1)
+	{
+		std::swap( x0 , x1 );
+		std::swap( y0 , y1 );
+	}
+
+
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+
+
+	int dy2 = std::abs( dy ) * 2;
+	int d = 0;
+	int y = y0;
+
+
+	for (int x = x0; x <= x1; x++)
+	{
+		if (steep)
+		{
+			SetPixel( y , x , col );
+		}
+		else
+		{
+			SetPixel( x , y , col );
+		}
+
+		d += dy2;
+		if (d > dx)
+		{
+			y += ( y1 > y0 ? 1 : -1 );
+			d -= dx * 2;
+		}
+	}
+}
+
--- Dazed/Rasterize.h
+++ Dazed/Rasterize.h
@@ -8,6 +8,12 @@ class Rasterize
 public:
 	static void SetPixel(int x,int y,const Color& col);
 
+	//DDA draw line func.
 	static void DrawLineDDA(const vec2i& p0,const vec2i& p1,const Color& col);
+
+	//Bresenham draw line func.
+	static void DrawLine(const vec2i& p0,const vec2i& p1,const Color& col);
+
+
 };
 
